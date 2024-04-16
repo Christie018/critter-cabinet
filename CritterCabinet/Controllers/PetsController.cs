5ac123d8dc92927407a17da7dcd837d1c9f01288
Christie018 <christie.sullivan018@gmail.com>
2024-04-16 01:46:40 -0400

    Initial commit

---
+++ CritterCabinet/Controllers/PetsController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CritterCabinet.Models;
+
+namespace CritterCabinet.Controllers
+{
+    public class PetsController : Controller
+    {
+        //Temp in-memory store
+        static List<Pet> _pets = new List<Pet>
+        {
+            new Pet {
+                Id = 1,
+                Name = "Max",
+                Type = "Dog",
+                Breed = "Labrador",
+                BirthDate = DateTime.Parse("2019-01-01"),
+                Status = "Adopted" },
+            new Pet {
+                Id = 2,
+                Name = "Whiskers",
+                Type = "Cat",
+                Breed = "Siamese",
+                BirthDate = DateTime.Parse("2020-02-01"),
+                Status = "New" },
+            new Pet {
+                Id = 3,
+                Name = "Goldie",
+                Type = "Fish",
+                Breed = "Goldfish",
+                BirthDate = DateTime.Parse("2021-03-01"),
+                Status = "In Recovery" }
+        };
+
+        //GET: Pets
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_pets);
+        }
+
+        //GET: Pets/Details/5
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var pet = _pets.FirstOrDefault(x => x.Id == id);
+            return View(pet);
+        }
+
+        //GET: Pets/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Pets/ Create
+        [HttpPost]
+        public IActionResult Create (Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                pet.Id = _pets.Max (x => x.Id) + 1;
+                _pets.Add(pet);
+
+                return RedirectToAction("Announcement",pet);
+            }
+
+            return View(pet);
+        }
+
+        public ActionResult Announcement(Pet newPet)
+        {
+            return View(newPet);
+        }
+    }
+}
---
+++ CritterCabinet/Models/Pet.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CritterCabinet.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Type is needed")]
+        public string Type { get; set; }
+
+        [StringLength(50, ErrorMessage = "Breed cannot be longer than 50 characters.")]
+        public string Breed { get; set; }
+
+        [Required(ErrorMessage = "Birthdate is required")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        public string Status {get; set; }
+    }
+}
